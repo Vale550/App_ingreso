e8da79de3125b566b1c92de7359823b667f2659d
VincentiJulieta <vincentijuli@gmail.com>
2023-03-01 11:54:50 -0300

    f

--- app/src/main/java/com/example/app_ingreso/Menu.java
+++ app/src/main/java/com/example/app_ingreso/Menu.java
@@ -4,30 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import org.w3c.dom.Text;
 
 
 public class Menu extends AppCompatActivity {
 
-    Text name,pass;
-    Button btning;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        btning = findViewById(R.id.btning);
-        name = findViewById(R.id.textname);
-        pass = findViewById(R.id.textpass);
 
-        btning.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
     }
 }
